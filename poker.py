f7af91bce515c24465e9ab0e3be36486d6af7b26
MGEternal <gmgame1020@outlook.com>
2023-08-11 19:48:40 +0700

    add compare function

--- poker.py
+++ poker.py
@@ -1,10 +1,11 @@
 import random
 
 # Define constants for the game
-SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+SUITS = ['Clubs','Hearts','Diamonds','Spades']
 RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 VALUES = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 11, 'Queen': 12, 'King': 13,
           'Ace': 14}
+
 
 # Define a Card class
 class Card:
@@ -28,7 +29,7 @@ class Deck:
 class Hand:
     def __init__(self):
         self.cards = []
-
+        self.chips = ''
     def add_card(self, card):
         self.cards.append(card)
 
@@ -42,43 +43,45 @@ class Card_table:
         self.cards.append(card)
 # Function to compare hands and determine the winner
 
-def compare_hands(hand1, hand2):
-    hand1_values = [VALUES[card.rank] for card in hand1.cards]
-    hand2_values = [VALUES[card.rank] for card in hand2.cards]
-    return sum(hand1_values) - sum(hand2_values)
 
 
 
 
-# Main game function
-def play_poker():
-    deck = Deck()
-    dict_players = dict
-    num_players = int(input("Number of players : "))
-    table = Card_table()
-    def add_card_to_hand(hand):
-        for _ in range(2):
-            hand.add_card(deck.deal_card())
-        return hand
+# Pregame function
+number_played = int(input("Number of players : "))
+player_list = []
+for i in range(number_played):
+    name = input(f'Name of player {i+1} : ')
+    player_dict = {'keys': name, 'values': ""}
+    player_list.append(player_dict)
+
+deck = Deck()
+
+for item in player_list:
+    pre_game_Hand = Hand()
+    pre_game_Hand.add_card(deck.deal_card())
+    item['values'] = str(pre_game_Hand)  # Store the string representation of the hand
+
+Set_buy_in = int(input("Set buy in of table : "))
+score_dict = {}
+
+for item in player_list:
+    hand_str = item['values']
+    hand_list = [Card(card_str.split()[0], card_str.split()[-1]) for card_str in hand_str.split(', ')]
+    if all(card.rank in VALUES for card in hand_list):  # Check if all card ranks are in VALUES
+        hand_suits = set(card.suit for card in hand_list)
+        if len(hand_suits) == 1:  # Check if all cards have the same suit
+            score_dict[item['keys']] = sum(VALUES[card.rank] for card in hand_list)
+
+# Sort the score_dict based on values
+sorted_score_list = sorted(score_dict, key=lambda x: (score_dict[x], x), reverse=True)
+
+print(str(player_list))
+print("Sorted score list:", ', '.join(sorted_score_list))
+
     
-    for i in range(num_players):
-        name = f'player{i}'
-        dict_players.append()
-    
-    for _ in range(3):
-        table.add_card(deck.deal_card())
-    
-    # Deal two cards to each player
     
 
-
-
-    # Compare hands to determine the winner
-    for i in table.cards:
-        print(i)
 
    
 
-# Start the game
-if __name__ == "__main__":
-    play_poker()
